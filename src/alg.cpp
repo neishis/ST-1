5455fb6867c170b024a5282ad80d5a5636e6265f
Victoria <vika.kondrashova.05@inbox.ru>
2026-02-16 12:58:35 +0300

    test

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,44 @@
 // Copyright 2025 UNN-CS
 #include <cstdint>
 #include "alg.h"
+#include <stdexcept>
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+   if (value <= 1) return false;
+  if (value <= 3) return true;
+  for(uint64_t i = 2; i < value; i++){
+    if (value % i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n < 1) throw std::invalid_argument("Аргумент не должен быть меньше 1");
+  uint64_t count = 0;
+  uint64_t var = 1;
+
+  while (count < n) {
+        var++;
+        if (checkPrime(var)) {
+            count++;
+        }
+    }
+  return var;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t var = value + 1;
+  while(!(checkPrime(var))){
+    var++;
+  }
+  return var;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+   uint64_t sum = 0;
+  for ( uint64_t i = 2; i < hbound; i++) {
+    if(checkPrime(i)) sum += i;
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -3,15 +3,60 @@
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
+#include <stdexcept>
 
 
 TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
+  uint64_t res = sumPrime(16);
+  uint64_t expected = 41;
   EXPECT_EQ(expected, res);
 }
 TEST(st1, sumPrime2) {
   uint64_t res = sumPrime(10);
   uint64_t expected = 17;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, sumPrime3) {
+  uint64_t res = sumPrime(6);
+  uint64_t expected = 10;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, checkPrime1) {
+  bool res = checkPrime(10);
+  EXPECT_EQ(false, res);
+}
+TEST(st1, checkPrime2) {
+  bool res = checkPrime(17);
+  EXPECT_EQ(true, res);
+}
+TEST(st1, checkPrime3) {
+  bool res = checkPrime(1);
+  EXPECT_EQ(false, res);
+}
+TEST(st1, checkPrime4) {
+  bool res = checkPrime(-16);
+  EXPECT_EQ(false, res);
+}
+TEST(st1, nPrime1) {
+  EXPECT_THROW(nPrime(0), std::invalid_argument);
+}
+TEST(st1, nPrime2) {
+  uint64_t res = nPrime(2);
+  uint64_t expected = 3;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, nPrime3) {
+  uint64_t res = nPrime(6);
+  uint64_t expected = 13;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, nextPrime1) {
+  uint64_t res = nextPrime(10);
+  uint64_t expected = 11;
+  EXPECT_EQ(expected, res);
+}
+TEST(st1, nextPrime2) {
+  uint64_t res = nextPrime(37);
+  uint64_t expected = 41;
   EXPECT_EQ(expected, res);
 }
